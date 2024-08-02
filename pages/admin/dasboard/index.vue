832781d0bb4601a8557c6b38adc90ff5d2ed8ef5
joxmal <joxmal123@gmail.com>
2024-08-02 08:01:46 -0400

    index login

--- pages/admin/dasboard/index.vue
+++ pages/admin/dasboard/index.vue
@@ -1,11 +1,26 @@
 <template>
     dasboard
+
+    <div v-if="loggedIn">
+      {{ token }}
+      <hr>
+      {{ user }}
+      <hr>
+      {{ loggedIn }}
+    </div>
 </template>
 
 
-<script setup>
+<script setup lang="ts">
 definePageMeta({
   middleware: 'auth'
 })
+
+interface User {
+  name: string
+}  
+
+const { user, loggedIn, token } = await useJwtAuth<User>()
+  console.log('user name is', user.value)
 
 </script>
